f75fb1bdcc6565d2df0e61242d229d5cf2a2f3df
Tim St. Clair <timsstclair@gmail.com>
2015-04-28 21:11:39 -0700

    scheme parsing

--- haskell/scheme/parsing.hs
+++ haskell/scheme/parsing.hs
@@ -1,5 +1,32 @@
-module Main where
-import qualified Text.ParserCombinators.Parsec as Parsec
+module Parsing where
 
-symbol :: Parsec.Parser Char
-symbol = Parsec.oneOf "!#$%&|*+-/:<=>?@^_~"
+import Text.ParserCombinators.Parsec (Parser, char, many, noneOf, oneOf, parse, skipMany1, space)
+
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right _ -> "Found value"
+
+main :: IO ()
+main = interact $ (++ "\n") . readExpr
